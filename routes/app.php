c0d3c0f3c4d50b9185a92804c71473d806bf11da
unknown <ardiandra45@gmail.com>
2022-04-09 23:30:51 +0700

    4/9/22

--- routes/app.php
+++ routes/app.php
@@ -4,7 +4,7 @@
 	
 	 
 		 Route::prefix('/api/app')->group(function () {
-            Route::get('/jeniskKendaraan', [Api\MerekKendaraan::class, 'index']);
+            Route::get('/jenisKendaraan', [Api\JenisKendaraan::class, 'index']);
             Route::get('/merekKendaraan', [Api\MerekKendaraan::class, 'index']);
             Route::get('/typeKendaraan', [Api\TypeKendaraan::class, 'index']);
             Route::get('/kondisiKendaraan', [Api\KondisiKendaraan::class, 'index']);
