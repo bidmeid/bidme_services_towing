026662604255e7a11cbd4f0b112559b0eb070c3d
unknown <ardiandra45@gmail.com>
2022-08-21 14:27:01 +0700

    8/21/2022

--- app/Http/Controllers/Auth/AuthCustomerController.php
+++ app/Http/Controllers/Auth/AuthCustomerController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\Tbl_customer as user;
+use App\Models\Tbl_customer as User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
