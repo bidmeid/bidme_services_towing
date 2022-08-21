daff05dff408405d93152078fa8ba58035c358ba
unknown <ardiandra45@gmail.com>
2022-08-22 01:27:38 +0700

    8/22/2022

--- routes/api.php
+++ routes/api.php
@@ -63,7 +63,7 @@ Route::group(['middleware' => 'auth:sanctum',], function () {
 		Route::post('/cancelOrder', [Api\PostOrder::class, 'cancelOrder']);
 		Route::post('/bidding', [Api\Bidding::class, 'index']);
 		Route::post('/myOrder', [Api\PostOrder::class, 'myOrder']);
-		Route::post('/orderById', [Api\PostOrder::class, 'checkOut']);
+		Route::post('/checkOut', [Api\PostOrder::class, 'checkOut']);
 		Route::post('/invoice', [Api\Invoice::class, 'index']);
 		Route::post('/updateAccount', [Api\UsersCustomer::class, 'update_account']);
 	});
