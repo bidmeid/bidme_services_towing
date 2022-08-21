f40fc0de4317f6994e8a942b43376709c5bcde91
unknown <ardiandra45@gmail.com>
2022-08-22 01:38:35 +0700

    8/22/2022

--- app/Http/Controllers/API/Mitra/Order.php
+++ app/Http/Controllers/API/Mitra/Order.php
@@ -17,15 +17,15 @@ class Order extends Controller
 
 	public function index(){
 		
-		$result = Tbl_order::where('orderStatus', 'proccess')->get();
+		$order = Tbl_order::where('orderStatus', 'proccess')->get();
 		
-		if((is_null($result)) OR ($result->count() == 0)){
+		if((is_null($order)) OR ($order->count() == 0)){
 			$message 	= 'Your request couldn`t be found';
 			return $this->sendResponseError($message, '',202);
 		}
 	     
 		$result = array();
-		foreach($result as $key=>$val){
+		foreach($order as $key=>$val){
 			$result[$key] = $val;
 			$result[$key]['customer'] = Tbl_customer::find($val->customerId);
 			$result[$key]['rute'] = Tbl_rute::find($val->ruteId);
