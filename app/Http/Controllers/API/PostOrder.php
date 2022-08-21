d5bc615ae1367bb9cc93109ec40daf7e68b62bcc
unknown <ardiandra45@gmail.com>
2022-08-22 00:53:51 +0700

    8/22/2022

--- app/Http/Controllers/API/PostOrder.php
+++ app/Http/Controllers/API/PostOrder.php
@@ -94,7 +94,7 @@ class PostOrder extends Controller
             return $this->sendResponseError(json_encode($validator->errors()), $validator->errors());       
         }
 		
-		$result = Tbl_order::where('customerId', Auth::user()->id)->where('id '. $request->orderId)->find();
+		$result = Tbl_order::where('customerId', Auth::user()->id)->where('id ', $request->orderId)->find();
 	
 		if((is_null($result)) OR ($result->count() == 0)){
 			$message 	= 'Your request couldn`t be found';
