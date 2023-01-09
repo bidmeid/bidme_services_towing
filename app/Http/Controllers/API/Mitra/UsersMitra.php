2d05a34b0a7eb9c4cc58560752d9c3bda98767c3
unknown <ardiandra45@gmail.com>
2023-01-09 13:28:59 +0700

    7-1-2023

--- app/Http/Controllers/API/Mitra/UsersMitra.php
+++ app/Http/Controllers/API/Mitra/UsersMitra.php
@@ -101,7 +101,7 @@ class UsersMitra extends Controller
 	public function saveToken(Request $request)
     {
         
-        $user = M_Users::where('id', $request->mitraId)->update(['device_token' => $request->token]);
+        $input = M_Users::where('id', $request->mitraId)->update(['device_token' => $request->token]);
         
 		if($input){
 			return $this->sendResponseCreate(null);
