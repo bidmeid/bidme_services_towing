652f44ebff73c3f62ae6442415564f07b7f6a53d
unknown <ardiandra45@gmail.com>
2023-02-02 18:51:49 +0700

    2-2-2023

--- app/Http/Controllers/API/PostOrder.php
+++ app/Http/Controllers/API/PostOrder.php
@@ -351,7 +351,16 @@ class PostOrder extends Controller
 	}
 	
 	public function CheckPaymentStatus($noInvoice) {
+		// Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
 		
+        \Midtrans\Config::$appendNotifUrl = url('api/payment-handler');
 		$status = \Midtrans\Transaction::status($noInvoice);
 		//$statuss = '';
 		$bank_acc = '';
