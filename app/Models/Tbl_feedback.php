79038d81da3fcacde63b35a60a6c11decc5e3515
unknown <ardiandra45@gmail.com>
2023-02-09 13:43:20 +0700

    9-2-2023

---
+++ app/Models/Tbl_feedback.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tbl_feedback extends Model
+{
+    protected $table = 'tbl_feedback';
+	
+	protected $primaryKey = 'id';
+	
+	protected $guarded = [];
+	
+	
+}
